056864aaf220f396d2e34cf3dd3da66e00aa87a8
Logan <lgwarlick@gmail.com>
2021-02-20 11:32:37 -0600

    Removed default set-up of app.component.html and app.component.ts and replaced them with application specifics

--- src/main/frontend/angularclient/src/app/app.component.ts
+++ src/main/frontend/angularclient/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //binds the component to the HTML template
   selector: 'app-root',
+
+  //where that HTML file is
   templateUrl: './app.component.html',
+
+  //the CSS that goes with that HTML file
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularclient';
+
+  //declaring a string variable
+  title: string;
+
+  //statically assigning title
+  constructor() {
+    this.title = 'Spring Boot - Angular Application'
+  }
 }
