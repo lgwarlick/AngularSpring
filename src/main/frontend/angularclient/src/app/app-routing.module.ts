9e617ba11d92fa4b52b1ddfa0ab591a720699e1b
Logan <lgwarlick@gmail.com>
2021-02-24 16:18:37 -0600

    updated app-routing.module.ts to handle reflect our components

--- src/main/frontend/angularclient/src/app/app-routing.module.ts
+++ src/main/frontend/angularclient/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserListComponent} from "./user-list/user-list.component";
+import {UserFormComponent} from "./user-form/user-form.component";
 
-const routes: Routes = [];
+//This directs which components to display when a link is clicked or a url is given
+//'path' is the string that matches the URL, component is the component that will be created in that case
+//e.g. /users will display UserListComponent
+const routes: Routes = [
+  {path: 'users', component: UserListComponent},
+  {path: 'adduser', component: UserFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
